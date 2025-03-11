c592328fdbd5e7cdcbb915d3b443fc22a5de6b6b
PRUTHUI <60943395+PRUTHUI@users.noreply.github.com>
2025-03-11 17:56:52 +0530

    Update AppMain.py

--- AppMain.py
+++ AppMain.py
@@ -1,5 +1,5 @@
 import datetime as dt
 def AppMain(): 
-    print('Message from code -- > deployment done V3.1 @ '+str(dt.datetime.now()))
+    print('Message from code -- > deployment done V4.0 @ '+str(dt.datetime.now()))
 AppMain()  
    
