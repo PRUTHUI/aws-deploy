41e6462db72ee187728a3db5a628307309319776
PRUTHUI <60943395+PRUTHUI@users.noreply.github.com>
2025-03-11 13:04:32 +0530

    Update AppMain.py

--- AppMain.py
+++ AppMain.py
@@ -1,11 +1,5 @@
-import snowflake.snowpark as snowpark
-from snowflake.snowpark import Session
-
+import datetime as dt
 def AppMain(): 
     print('deployment done v1 @ '+str(dt.datetime.now()))
-    session=aws_secret_manager.create_session()
-    sql="create or replace table DEV_DS_DB._FORECAST_APP.log1 as select 'test_message v1' from dual"
-    message=session.sql(sql).collect()
-    
 AppMain()  
    
